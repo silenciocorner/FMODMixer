683cd104947d9cf29eb2f1a9a9ace88e63a34d43
momnus <momnus@ya.ru>
2021-03-25 10:56:47 +0300

    FMOD Mixer

---
+++ Assets/Scripts/Snapshot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snapshot : MonoBehaviour
+{
+    FMOD.Studio.EventInstance snapshot;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            snapshot = FMODUnity.RuntimeManager.CreateInstance("snapshot:/FilterMusic");
+            snapshot.start();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            snapshot.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            snapshot.release();
+        }
+    }
+}
---
+++ Assets/Scripts/VCA.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VCA : MonoBehaviour
+{
+    FMOD.Studio.EventInstance instance;
+    FMOD.Studio.VCA vca;
+
+    [FMODUnity.EventRef]
+    public string fmodEvent;
+
+    [SerializeField]
+    [Range(-80f, 10f)]
+    private float vcaVolume;
+
+    private float volume;
+
+    void Start()
+    {
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+
+        vca = FMODUnity.RuntimeManager.GetVCA("vca:/MusicVCA");
+    }
+    void Update()
+    {
+        volume = Mathf.Pow(10.0f, vcaVolume / 20f);
+        vca.setVolume(volume);
+    }
+}
